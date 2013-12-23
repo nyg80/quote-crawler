946685690e5f1e711fa65a2d7a1f6359ea00c508
nyg80 <nyg80@server.fake>
2013-12-22 23:55:38 -0500

    Crawling part finished, next piece is to add the business logic and web searching

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+This script requires that beautifulsoup is installed.  To install this, run sudo apt-get install python-bs4 .
+
+This script is similar to some things I had to build at an old job, although those scripts were written in PHP using the simple_html_dom.php library, and then later on I rewrote them to use the QueryPath library.  I also wrote similar crawling scripts using Perl, WWW::Mechanize, URI::http and Text::CSV to crawl inputted spreadsheets looking for certain things on pages.
--- quote_crawl.py
+++ quote_crawl.py
@@ -1,9 +1,16 @@
-#from BeautifulSoup import BeautifulSoup
-from HTMLParser import HTMLParser
+from bs4 import BeautifulSoup
+#from HTMLParser import HTMLParser
 import urllib2
 import re
 
 inputFormat = re.compile("^http[s]*://.+\..+$")
+
+
+#using dictionaries which act like hashes, so that the key will contain our lowercase, full-length
+#URL and the value will contain the (potentially mixed-case) URL we found on the page
+#this way, we can makes requests for mixed case URLs so that IIS servers won't 404 on us unnecessarily
+toVisit = {}
+visitedPages = {}
 
 #get the input domain from the user, do some basic validation on it
 while 1:
@@ -11,8 +18,39 @@ while 1:
 	if inputFormat.match(domainToCrawl):
 		break
 
-#To Do:
+#To Do: make URL comparisons case insensitive, but keep requested URLs mixed-case so that IIS servers dont give 404s on us
+
+
 #get the page, all internal links add to internal links hash
+response = urllib2.urlopen(domainToCrawl)
+pageSrc = response.read()
+
+soup = BeautifulSoup(pageSrc)
+
+while True:
+	for link in soup.find_all('a'):
+		#convert relative links to absolute links, so we can determine internal vs external links
+		if link.get('href') is not None:
+			if link.get('href').startswith("/"):
+				linkAddr = domainToCrawl + link.get('href')
+			else:
+				linkAddr = link.get('href')
+			if domainToCrawl in linkAddr:
+				if (linkAddr not in visitedPages) and (not linkAddr.endswith(".pdf")):
+					toVisit[linkAddr] = link.get('href')
+					#print "added %s to toVisit" % linkAddr
+	nextPage = toVisit.popitem()
+	soup = BeautifulSoup(urllib2.urlopen(nextPage[0]).read())
+	visitedPages[nextPage[0]] = nextPage[1]
+	if not toVisit:
+		break
+
+print visitedPages
+
+#links = soup.find_all('a')
+
+#print links
+
 
 
 #get a random URL from the toVisit hash, get that page
@@ -22,10 +60,9 @@ while 1:
 
 #http://docs.python.org/2/library/urlparse.html
 
-response = urllib2.urlopen(domainToCrawl)
-pageSrc = response.read()
 
-print pageSrc
+
+#print pageSrc
 
 #response = urllib2.urlopen("http://google.de")
 #page_source = response.read()
